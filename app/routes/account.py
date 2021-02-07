54b6613a6cd6bdc9802667caaa4e3ea1f8ad488c
epohs <blurryphoto@gmail.com>
2021-02-07 17:20:51 +0000

    Restructuring app directory

---
+++ app/routes/account.py
@@ -0,0 +1,55 @@
+from config import AdminUser
+from app import app
+from app.forms import LoginForm
+from flask import render_template, flash, url_for, redirect, request
+from flask_login import current_user, login_user, logout_user
+
+
+
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+
+    if current_user.is_authenticated:
+        return redirect(url_for('index'))
+
+    form = LoginForm()
+
+    if request.method == 'POST' and form.validate_on_submit():
+        
+        user = User.query.filter_by(username=form.username.data).first()
+        
+        if user is None or not user.check_password(form.password.data):
+
+            flash('Invalid username or password')
+
+        else:
+
+            login_user(user, remember=form.remember_me.data)
+            return redirect(url_for('index'))
+
+    elif request.method == 'POST':
+
+        flash('Something wasn’t right')
+
+
+    return render_template('login.html', title='Sign In', form=form)
+
+
+    
+    
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+
+
+
+@app.route('/setup')
+def setup():
+    return render_template('setup.html', title='Setup', user=AdminUser)
+
+    
