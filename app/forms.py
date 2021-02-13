33e9851f4a0a9e93babbe740a17c6bff8d1a51e4
epohs <blurryphoto@gmail.com>
2021-02-13 20:31:54 +0000

    added new entry form

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,15 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.fields.html5 import DecimalRangeField
 from wtforms.validators import DataRequired
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
-    password = PasswordField('Password', validators=[DataRequired()])
-    remember_me = BooleanField('Remember Me')
-    submit = SubmitField('Sign In')
+  username = StringField('Username', validators=[DataRequired()])
+  password = PasswordField('Password', validators=[DataRequired()])
+  remember_me = BooleanField('Remember Me')
+  submit = SubmitField('Sign In')
+    
+    
+class NewEntry(FlaskForm):
+  val = DecimalRangeField('val', places="1", validators=[DataRequired()])
+  submit = SubmitField('Add entry')
--- app/routes/entries.py
+++ app/routes/entries.py
@@ -1,6 +1,11 @@
-from app import app
+from app import app, db
+from app.forms import NewEntry
+from app.models import Entry
 from flask import render_template, flash, url_for, redirect, request
 
 @app.route('/new')
 def new_entry():
-  return render_template('entry/new.html', title='New entry')
+  
+  form = NewEntry()
+  
+  return render_template('entry/new.html', title='New entry', form=form)
