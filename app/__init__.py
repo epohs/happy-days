8d4d7a2865a9a012a0ad866debc62d7fa4221e1a
epohs <blurryphoto@gmail.com>
2021-02-14 17:27:41 +0000

    moved static files into app

--- app/__init__.py
+++ app/__init__.py
@@ -13,7 +13,7 @@ login = LoginManager(app)
 assets = Environment(app)
 assets.debug = Config.DEBUG
 
-css = Bundle('reset.css', 'fonts.css', 'base.css', 'responsive.css',
+css = Bundle('css/reset.css', 'css/fonts.css', 'css/base.css', 'css/responsive.css',
             filters='cssmin', output='packed/app.css')
 
 assets.register('css_all', css)
