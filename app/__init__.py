f12fd041fcf91ac49fd0a72a6c73de7addef2121
epohs <blurryphoto@gmail.com>
2021-02-14 02:47:32 +0000

    add flask-asset and debug from config

--- app/__init__.py
+++ app/__init__.py
@@ -3,15 +3,19 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from flask_assets import Environment, Bundle
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 login = LoginManager(app)
+assets = Environment(app)
+assets.debug = Config.DEBUG
 
 from app import routes, models
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+  
+    app.run( debug = Config.DEBUG )
