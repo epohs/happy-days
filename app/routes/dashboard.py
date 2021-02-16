03fd9fbe710f7e6f7f1737429715004d1dc74fad
epohs <blurryphoto@gmail.com>
2021-02-16 03:04:44 +0000

    playing with days_ago empty placeholder functionality

--- app/routes/dashboard.py
+++ app/routes/dashboard.py
@@ -5,6 +5,7 @@ from flask import render_template, flash, url_for, redirect, request
 from flask_login import current_user
 from sqlalchemy import func
 from datetime import datetime, timedelta 
+import math
 
 @app.route('/dashboard')
 def dashboard():
@@ -44,6 +45,22 @@ def dashboard():
       parent = 0,
       entry_type_id = 0
     ).order_by(day.desc()).group_by(day).all()
+    
+  
+  #entries_map = [x for x in recent_entries if math.floor(x.days_ago) == 2]
+
+  
+  #flash( team_map )
+  
+  # Add emtpy flag
+  # for entry in recent_entries: 
+  for day_ago in reversed(range(30 + 1)): 
+    #date = entry.__dict__['days_ago']
+    #i.__dict__['date_modified'] = date.replace('/', '-')
+    
+    if ( not len([x for x in recent_entries if math.floor(x.days_ago) == day_ago]) ):
+      
+      flash('Nothing {} days ago'.format(day_ago))
   
   
   return render_template('dashboard.html', title='Dashboard', recent_entries=recent_entries)
