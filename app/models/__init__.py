a02b1bced13c458bb096da9565325f916c1bff77
epohs <blurryphoto@gmail.com>
2021-02-17 11:48:07 -0500

    adding parent child relationships

--- app/models/__init__.py
+++ app/models/__init__.py
@@ -28,8 +28,8 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(120), index=True, unique=True)
     password = db.Column(db.String(128))
     user_level = db.Column(db.Integer, index=False, unique=False, nullable=True, default=0)
-    created_on = db.Column(db.DateTime, index=False, unique=False, nullable=True,  default=datetime.utcnow)
-    last_login = db.Column(db.DateTime, index=False, unique=False, nullable=True)
+    created_on = db.Column(db.DateTime(), index=False, unique=False, nullable=True,  default=datetime.now)
+    last_login = db.Column(db.DateTime(), index=False, unique=False, nullable=True,  default=datetime.now)
     entries = db.relationship('Entry', backref='author', lazy='dynamic')
 
     def __repr__(self):
@@ -51,11 +51,17 @@ class User(UserMixin, db.Model):
 class Entry(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     val = db.Column(db.Float)
-    entry_type_id = db.Column(db.Integer, nullable=False, default=0)
-    parent = db.Column(db.Integer, unique=False, nullable=False, default=0)
-    created_on = db.Column(db.DateTime, index=False, unique=False, nullable=True,  default=datetime.utcnow)
-    modified_on = db.Column(db.DateTime, index=False, unique=False, nullable=True,  default=datetime.utcnow)
+    entry_type = db.Column(db.Integer, nullable=False, default=0)
+    parent_id = db.Column(db.Integer, db.ForeignKey('entry.id'), default=0)
+    parent = db.relationship("Entry",
+                        remote_side=[id],
+                        primaryjoin=('entry.c.id==entry.c.parent_id'),
+                        backref="children",
+                        lazy="joined")
+    created_on = db.Column(db.DateTime(), index=False, unique=False, nullable=True,  default=datetime.now)
+    modified_on = db.Column(db.DateTime(), index=False, unique=False, nullable=True,  default=datetime.now)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    
 
     def __repr__(self):
         return '<Entry {}>'.format(self.val)
